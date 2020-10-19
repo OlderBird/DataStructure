c28047466dd6158b36aa84d0b756a70916ea5a2c
aiqi_glb <493147794_glb@qq.com>
2020-10-19 13:31:30 +0800

    逆波兰

--- java/C_单向链表.java
+++ java/C_单向链表.java
@@ -4,10 +4,10 @@ package cn.aq;
 public class C_单向链表 {
     public static void main(String[] args) {
 
-        List list1 = new List(1,"艾琦");
-        List list2 = new List(2,"蕾姆");
-        List list3 = new List(4,"拉姆");
-        List list4 = new List(5,"迪奥");
+        ListNode list1 = new ListNode(1,"艾琦");
+        ListNode list2 = new ListNode(2,"蕾姆");
+        ListNode list3 = new ListNode(4,"拉姆");
+        ListNode list4 = new ListNode(5,"迪奥");
 //        创建链表
         CreateList createList = new CreateList();
         createList.insertList(list1);
@@ -15,7 +15,7 @@ public class C_单向链表 {
         createList.insertList(list3);
         createList.insertList(list4);
 //        按编号添加
-        List list5 = new List(3,"鸡掰");
+        ListNode list5 = new ListNode(3,"鸡掰");
         createList.insertListById(list5);
 //        删除节点
         createList.delListByIndex(5);
@@ -27,12 +27,12 @@ public class C_单向链表 {
 }
 
 //1、定义 List，每个 List 对象就是一个节点
-class List {
+class ListNode {
     public int id;      //节点代号
     public String data; //节点数据
-    public List next;
+    public ListNode next;
 
-    List(int id, String data) {
+    ListNode(int id, String data) {
         this.id = id;
         this.data = data;
     }
@@ -49,17 +49,17 @@ class List {
 //2、定义 List 管理链表
 class CreateList {
 /**    1、先初始化一个头节点，头节点不要动，不存放具体的数据*/
-    private List head = new List(0,"head");
+    private ListNode head = new ListNode(0,"head");
 
 /**    返回头节点*/
-    public List getHead() {
+    public ListNode getHead() {
         return head;
     }
 
 /**    2、添加节点到单向链表
  *      思路：找到当前链表的最后节点，然后将最后节点的 next 指向最新的节点*/
-    public void insertList (List newList) {
-        List p = head;       //因为头节点不能动，我们需要一个临时变量
+    public void insertList (ListNode newList) {
+        ListNode p = head;       //因为头节点不能动，我们需要一个临时变量
         while (true) {          //遍历链表，找到Uzi后
             if (p.next == null)
                 break;
@@ -70,8 +70,8 @@ class CreateList {
 
 /**    3、按照编号顺序添加节点
  *      思路：首先找到新添加节点的位置，然后根据c++的方式添加节点*/
-    public void insertListById (List newList) {
-        List p = head;
+    public void insertListById (ListNode newList) {
+        ListNode p = head;
         while (p.next != null) {
             if (p.next.id > newList.id)
                 break;
@@ -84,7 +84,7 @@ class CreateList {
 /**    4、删除节点
  *      被删除的节点 会被垃圾回收机制回收*/
     public void delListByIndex(int id) {
-        List p = head.next;
+        ListNode p = head.next;
         while (p.next != null) {
             if (p.next.id == id) {
                 p.next = p.next.next;
@@ -97,7 +97,7 @@ class CreateList {
 
 /**    5、显示链表，遍历*/
     public void showList () {
-        List p = head.next;
+        ListNode p = head.next;
         while (true) {
             if (p.next == null) {
                 System.out.println(p);
@@ -114,14 +114,14 @@ class CreateList {
  *          1、先定义一个节点 reverseHead = new List();
  *          2、从头到尾遍历原来的节点，每遍历一个节点，就将其取出，并放在心的链表 reverseHead 的最前端
  *          3、原来的链表的 head.next = reverseHead.next*/
-        public static void reverseList (List head) {
+        public static void reverseList (ListNode head) {
 //            如果当前链表为空，或只有一个节点，无需反转，直接返回
             if (head.next == null || head.next.next == null)
                 return;
 //            遍历原来的链表
-            List originHead = head.next;
-            List next = null;   //指向当前节点【originHead】的下一个节点
-            List reverseHead = new List(0,"");
+            ListNode originHead = head.next;
+            ListNode next = null;   //指向当前节点【originHead】的下一个节点
+            ListNode reverseHead = new ListNode(0,"");
 
             while (originHead != null) {
                 next = originHead.next;  //先暂时保存当前节点的下一个节点，因为后面需要使用
---
+++ java/H_逆波兰计算器.java
@@ -0,0 +1,57 @@
+package cn.aq;
+
+/*思路：1、先将后缀表达式放入到ArrayList中
+*      2、在将ArrayList传递给一个方法遍历ArrayList配合栈完成计算*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+//逆波兰计算器
+public class H_逆波兰计算器 {
+    public static void main(String[] args) {
+//        先定义给逆波兰表达式
+//        （3+4)×5-6 = 34+5×6-
+//        使用java内置的栈
+        String expression = "3 4 + 5 × 6 -";
+        List<String> list = getExpression(expression);
+        int result = calculate(list);
+        System.out.println(result);
+
+    }
+
+    public static List<String> getExpression(String expression) {
+        String[] split = expression.split(" ");
+        List<String> list = new ArrayList<>();
+        for (String ele : split)
+            list.add(ele);
+        return list;
+    }
+
+    public static int calculate(List<String> list) {
+        Stack<String> stack = new Stack<>();    //java内置栈
+        for (String l : list) {
+            if (l.matches("\\d+"))  //匹配的是多位数
+                stack.push(l);
+            else {
+                int num1 = Integer.parseInt(stack.pop());
+                int num2 = Integer.parseInt(stack.pop());
+                int result;
+                if (l.equals("+"))
+                    result = num1 + num2;
+                else if (l.equals("-"))
+                    result = num2 - num1;
+                else if (l.equals("×"))
+                    result = num1 * num2;
+                else if (l.equals("÷"))
+                    result = num2 / num1;
+                else
+                    throw new RuntimeException("运算符有误");
+                stack.push(String.valueOf(result));
+            }
+        }
+//        最后留在 stack 栈中的数据是运算结果
+        return Integer.parseInt(stack.pop());
+    }
+
+}
